003bb278a0ceabe4af6400da8465facffbc0ea8c
sunshineQK <893495453@qq.com>
2025-07-03 11:03:17 +0800

    1

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "musixuser";
+// MySQL Database Password
+$sql_db_pass = "i5.Z)S9w!(vc*)E(";
+// MySQL Database Name
+$sql_db_name = "musix";
+
+// Site URL
+$site_url = "http://localhost"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "aaa35d56-3039-46a6-abae-d1adee3bd62e"; // Your purchase code, don't give it to anyone.
+?>
